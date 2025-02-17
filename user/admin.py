41485b717abe1dc8648019e4b0211e8e5196fbbe
Arsenii Aristov <arseniyaristov07@gmail.com>
2025-02-17 09:34:46 +0200

    solution

--- user/admin.py
+++ user/admin.py
@@ -1,18 +1,15 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 from django.utils.translation import gettext as _
-from user.models import *
-
+from .models import User
 
 @admin.register(User)
-class UserAdmin(DjangoUserAdmin):
+class CustomUserAdmin(DjangoUserAdmin):
     fieldsets = (
         (None, {"fields": ("username", "password")}),
         (_("Personal info"), {"fields": ("first_name", "last_name")}),
         (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser",
-                                       "groups", "user_permissions")}),
+                                        "groups", "user_permissions")}),
         (_("Important dates"), {"fields": ("last_login", "date_joined")}),
     )
 
