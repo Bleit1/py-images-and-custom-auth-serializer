a68819ed6168686b678053bd41427b19fd5fdb16
Arsenii Aristov <arseniyaristov07@gmail.com>
2025-02-17 09:31:22 +0200

    solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -163,4 +163,3 @@ class OrderSerializer(serializers.ModelSerializer):
 
 class OrderListSerializer(OrderSerializer):
     tickets = TicketListSerializer(many=True, read_only=True)
-
--- user/admin.py
+++ user/admin.py
@@ -9,21 +9,20 @@ from user.models import *
 @admin.register(User)
 class UserAdmin(DjangoUserAdmin):
     fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
-                                       'groups', 'user_permissions')}),
-        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name")}),
+        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser",
+                                       "groups", "user_permissions")}),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
     )
 
     add_fieldsets = (
         (None, {
-            'classes': ('wide',),
-            'fields': ('username', 'first_name', 'last_name'),
+            "classes": ("wide",),
+            "fields": ("username", "first_name", "last_name"),
         }),
     )
 
-    list_display = ('username', 'first_name', 'last_name', 'is_staff')
-    search_fields = ('username', 'first_name', 'last_name')
-    ordering = ('email',)
-
+    list_display = ("username", "first_name", "last_name", "is_staff")
+    search_fields = ("username", "first_name", "last_name")
+    ordering = ("email",)
